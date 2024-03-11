f4b96582ffd5269f5c9e1987946f0e98a3a9032f
Iugal23 <132124608+Iugal23@users.noreply.github.com>
2024-03-11 12:29:32 +0100

    Update test_bench_bot.py

--- test_bench_bot.py
+++ test_bench_bot.py
@@ -1,16 +1,22 @@
 import pyautogui
 import time
-time.sleep(5)
-# apri la console di windows nella cartella del progetto
+# apri la console di windows nella cartella del progetto e lancia il programma
 # il codice usa vivado in modalità tcl per poter effettuare le simulazioni
-pyautogui.write("vivado -mode tcl open_project project_reti_logiche.xpr")
-pyautogui.press("enter")
 
-for i in range(0,1000):
+NUMBER_OF_TB=1000
+time.sleep(5)
+pyautogui.write("vivado -mode tcl ")
+pyautogui.press("enter")
+pyautogui.write("open_project project_reti_logiche.xpr")
+pyautogui.press("enter")
+time.sleep(3)
+
+for i in range(0,NUMBER_OF_TB):
     pyautogui.write(f"set_property top project_tb_{i} [get_filesets sim_1]")
     time.sleep(2)
     pyautogui.press("enter")
-    pyautogui.write("launch_simulation -mode post-synthesis -type functional ")
+    pyautogui.write("launch_simulation -mode post-synthesis -type functional")
     time.sleep(1)
     pyautogui.press("enter") 
     time.sleep(15)
+# nel file vivado.log trovi i risultati delle simulazioni
